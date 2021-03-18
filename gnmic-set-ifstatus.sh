ceef6142de54e435d0a6a136529e9496b2eb5654
Jeroen van Bemmel <jvb127@gmail.com>
2021-03-18 12:54:00 -0500

    Update gnmic-set-ifstatus.sh

--- gnmic-set-ifstatus.sh
+++ gnmic-set-ifstatus.sh
@@ -6,6 +6,6 @@ ADMINSTATUS="$2"
 GNMIC="/usr/local/bin/gnmic -a 127.0.0.1:57400 -u admin -p admin --skip-verify -e json_ietf"
 IFNAME=`$GNMIC get --path /interface/ifindex | \
         jq -r --arg i "$IFINDEX" '.[].updates[].values[""]["srl_nokia-interfaces:interface"][] | select (.ifindex == $i)|.name'`
-$GNMIC set --update-path /interface[name="$IFNAME"]/admin-state --update-value $ADMINSTATUS
+[[ "$IFNAME" != "" ]] && $GNMIC set --update-path /interface[name="$IFNAME"]/admin-state --update-value $ADMINSTATUS
 
 exit $?
