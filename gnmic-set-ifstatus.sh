e32fe3f6aff5e864d1d1ccfecb2a2a1c9404dec4
Jeroen van Bemmel <jvb127@gmail.com>
2021-03-18 12:36:37 -0500

    Update gnmic-set-ifstatus.sh

--- gnmic-set-ifstatus.sh
+++ gnmic-set-ifstatus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script parameters: SNMP_ifIndex adminStatus(1=up or 2=down)
+# Script parameters: SNMP_ifIndex adminStatus('enabled' or 'disabled')
 IFINDEX="$1"
 ADMINSTATUS="$2"
 GNMIC="/usr/local/bin/gnmic -a 127.0.0.1:57400 -u admin -p admin --skip-verify -e json_ietf"
