160fa6a73857f594353ea3cacb45b56445e161e4
Jeroen van Bemmel <jvb127@gmail.com>
2021-03-18 12:04:32 -0500

    Create gnmic-set-ifstatus.sh

---
+++ gnmic-set-ifstatus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script parameters: SNMP_ifIndex adminStatus(1=up or 2=down)
+IFINDEX="$1"
+ADMINSTATUS="$2"
+GNMIC="/usr/local/bin/gnmic -a 127.0.0.1:57400 -u admin -p admin --skip-verify -e json_ietf"
+IFNAME=`$GNMIC get --path /interface/ifindex | \
+        jq -r ".[].updates[].values['']['srl_nokia-interfaces:interface'][] | select (.ifindex == $IFINDEX)|.name"`
+$GNMIC set --update-path /interface[name="$IFNAME"]/admin-state --update-value $ADMINSTATUS
