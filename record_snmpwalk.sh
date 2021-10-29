69cff3014c606853c7f93bc9165a66e7fa5af501
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-10-29 09:35:29 -0500

    Wait longer

--- record_snmpwalk.sh
+++ record_snmpwalk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-byzanz-record --delay=0 --exec "xterm -maximize -e 'sleep 1 && cat do_snmpwalk.sh && ./do_snmpwalk.sh && sleep 5'" snmpwalk_bgp.gif
+byzanz-record --delay=0 --exec "xterm -maximize -e 'sleep 1 && cat do_snmpwalk.sh && ./do_snmpwalk.sh && sleep 10'" snmpwalk_bgp.gif
