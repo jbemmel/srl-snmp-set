24c319fae2aa60a78f693e11f8c0965080bf6ab6
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-10-29 09:30:42 -0500

    Add show command

--- do_snmpwalk.sh
+++ do_snmpwalk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# on leaf1
-snmpwalk -v 2c -c private 172.20.20.3 1.3.6.1.2.1.15
+ssh admin@clab-snmp-set-lab-leaf1 <<< "show network-instance default protocols bgp neighbor"
+
+# Walk BGP4 MIB on leaf1
+snmpwalk -v 2c -c private clab-snmp-set-lab-leaf1 1.3.6.1.2.1.15
